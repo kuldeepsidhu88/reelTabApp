0ad90f92c6cdfd2d9d7569ae92b2b92f684f5564
kuldeepsidhu88 <mktsywr@gmail.com>
2016-03-26 18:25:10 +0530

    Added recentPostsList controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,34 @@
 angular.module('app.controllers', [])
 
 // recentpostlist controller  
-.controller('recentPostsListCtrl', function($scope) {
-
+.controller('recentPostsListCtrl', function($scope,$http) {
+	var recentpostslist = [];
+	// TODO: get posts
+	var getPostsURL = "http://www.punjabireel.com/wp-json/wp/v2/posts";
+	$http.get(getPostsURL)
+        .success(function(response) {
+            console.log(response);
+            for(var i in response){
+            	var post ={};
+	        	var type = response[i].type;
+	        	var id = response[i].id;
+	        	var titleRendered = response[i].title.rendered;
+				if(type=='post'){
+					post['id']=id;
+					post['title']=titleRendered;
+					recentpostslist.push(post);
+				}
+            }
+        })
+        .error(function(error){
+        	console.log("Error Occured. "+error);
+        });
+    $scope.recentpostslist = recentpostslist;
 })
 
 // authorslist controller
 .controller('authorsListCtrl', function($scope) {
-
+ // http://punjabireel.com/wp-json/wp/v2/posts?filter[poet]=
 })
 
 // authorpostlist contoller
@@ -35,7 +56,6 @@ angular.module('app.controllers', [])
 					category['count']=count;
 					categories.push(category);
 				}
-				
             }
         })
         .error(function(error){
@@ -59,8 +79,8 @@ angular.module('app.controllers', [])
             console.log(response);
             for(var i in response){
             	var post ={};
-            	var id = response[i].id;
             	var type = response[i].type;
+            	var id = response[i].id;
             	var titleRendered = response[i].title.rendered;
             	var title = titleRendered.replace("&#8211;","-");
             	if(type=='post'){
@@ -84,8 +104,32 @@ angular.module('app.controllers', [])
 })
 
 // post detail controller   
-.controller('postDetailCtrl', function($scope) {
+.controller('postDetailCtrl', function($scope,$stateParams,$http) {
+	var postId = $stateParams.postId;
 
+	// TODO: get post detail 
+	var getPostDetailURL = "http://punjabireel.com/wp-json/wp/v2/posts/"+postId;
+	$http.get(getPostDetailURL)
+        .success(function(response) {
+            console.log(response);            
+        	var post ={};
+        	var type = response.type;
+        	var id = response.id;
+        	var titleRendered = response.title.rendered;
+        	var link = response.link;
+        	var contentRendered = response.content.rendered;
+        	
+        	if(type=='post'){
+				post['id']=id;
+				post['title']=titleRendered;
+				post['link']=link;
+				post['content']=contentRendered;
+				$scope.post = post;
+			}
+        })
+        .error(function(error){
+        	console.log("Error Occured. "+error);
+        });
 })
 
 
